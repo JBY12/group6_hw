a532ed4df24a7604f0795d44e9959a3d28a86b34
Gazizov Aydar <aydar_gazizov@mail.ru>
2021-10-15 23:01:06 +0500

    29.09.21 hw

---
+++ 29_09_21 hw2/index.php
@@ -0,0 +1,34 @@
+<?
+
+$a=false || true;
+$b=false or true;
+var_dump($a, $b);
+
+$c=false && true;
+$d=false and true;
+var_dump($c, $d);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+    content="width=device-width", user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Title</title>
+</head>
+<body>
+<? echo $a ?>
+<div>
+    <div>
+        <p>Hello!</p>
+        <p>Hello!</p>
+        <p>Bye</p>
+
+        <p>Hello!</p>
+        <p>Hello!</p>
+    </div>
+</div>
+
+</body>
+</html>
